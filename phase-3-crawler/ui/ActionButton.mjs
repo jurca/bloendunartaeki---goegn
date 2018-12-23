aa1d643906041aa22f9e759d97819bdb4be47f9b
Martin Jurča <mjurca@centrum.cz>
2018-12-23 18:08:50 +0100

    added a custom element for action buttons using ignore.js

---
+++ phase-3-crawler/ui/ActionButton.mjs
@@ -0,0 +1,27 @@
+import Component from '../node_modules/@jurca/-x-ignore/Component.js'
+import {define} from '../node_modules/@jurca/-x-ignore/ignore.js'
+import {renderToDom} from '../node_modules/@jurca/-x-ignore/naiveRenderer.js'
+
+export default class ActionButton extends Component {
+    constructor(rootNode) {
+        super(renderToDom)
+
+        this._rootNode = rootNode
+    }
+
+    render() {
+        const type = this.props.type || this._rootNode.getAttribute('type')
+
+        return `
+            <style>
+                @import 'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css';
+            </style>
+            <button class="btn${type ? ` btn-${type}` : ''}">
+                <slot/>
+            </button>
+        `
+    }
+}
+ActionButton.props = ['type']
+
+define('action-button', ActionButton)
